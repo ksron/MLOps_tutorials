773a7151bc4cd5757140eb3a36b12b7d0a414737
Saron Kim <kasrlmoojoo@gmail.com>
2024-03-20 10:31:21 +0900

    initial client created

---
+++ 2_nlp_model_serving/mlops-library/mlopslib/mlops_gcp_client.py
@@ -0,0 +1,9 @@
+class MLOpsGCSClient(object):
+    def __init__(self) -> None:
+        return True
+    
+    def upload_model():
+        return True
+    
+    def dowload_model():
+        return True
