92f972c210f1c05c3eb961825f88fec0633af2de
Saron Kim <kasrlmoojoo@gmail.com>
2024-03-21 09:10:34 +0900

    packaging info added

---
+++ 2_nlp_model_serving/mlops-library/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="nlp_mlopslib",
+    version="0.0.1",
+    description="MLOps library for NLP model",
+    url="git@github.com:ksron/MLOps_tutorials.git",
+    author="ksron",
+    packages=["mlopslib"],
+    install_requires=[
+        "google-cloud-storage==2.16.0"
+    ]
+)
